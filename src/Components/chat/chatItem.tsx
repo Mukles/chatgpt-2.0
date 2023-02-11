a9ee5ecb4fa9a0b1adf4122d29e11cc5677bc897
Mukles <Mukleshossen1763@gmail.com>
2023-02-11 17:04:57 +0600

    profile add

--- src/Components/chat/chatItem.tsx
+++ src/Components/chat/chatItem.tsx
@@ -1,4 +1,5 @@
-import { Like, Unlike } from "../../icons/Icons";
+import defaultProfile from "../../assets/default.jpg";
+import { ChatGpt, Like, Unlike } from "../../icons/Icons";
 
 interface Props {
   text?: any;
@@ -9,18 +10,20 @@ const ChatItem = ({ text, i }: Props) => {
   return (
     <div className={`chat-wrapper ${i % 2 === 0 ? "gpt" : ""}`}>
       <div className="chat">
-        <div className="profile"></div>
+        <div className="profile">
+          {i % 2 === 0 ? <ChatGpt /> : <img src={defaultProfile} alt="" />}
+        </div>
         <div className="content">
           <p>Text {text}</p>
-        </div>
 
-        <div className="icons">
-          <button type="button">
-            <Like />
-          </button>
-          <button type="button">
-            <Unlike />
-          </button>
+          <div className="icons">
+            <button type="button">
+              <Like />
+            </button>
+            <button type="button">
+              <Unlike />
+            </button>
+          </div>
         </div>
       </div>
     </div>
